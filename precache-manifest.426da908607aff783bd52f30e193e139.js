a9bf8773c01bcdc506821b81dae91b97adfbc2a5
Diego <didachos24@gmail.com>
2019-04-27 03:00:23 -0500

    Updates

---
+++ precache-manifest.426da908607aff783bd52f30e193e139.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bfcdbd6341a3aaa7651859286afa7e",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "67d7b2830b298bb25324",
+    "url": "/memorygame/static/css/main.5cb0c22a.chunk.css"
+  },
+  {
+    "revision": "55d0e06cb8c6e71dd7f6",
+    "url": "/memorygame/static/js/2.137c14e4.chunk.js"
+  },
+  {
+    "revision": "67d7b2830b298bb25324",
+    "url": "/memorygame/static/js/main.cc3cac81.chunk.js"
+  },
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  },
+  {
+    "revision": "d32807fb626d788865ccbaebaf66dfbc",
+    "url": "/memorygame/static/media/Frieza.d32807fb.png"
+  },
+  {
+    "revision": "645cfc370068a61026788b8eee6e5488",
+    "url": "/memorygame/static/media/Son_Gohan.645cfc37.png"
+  },
+  {
+    "revision": "cfe7289ecdd783ec3621004097bcc224",
+    "url": "/memorygame/static/media/Vegeta_DBZ.cfe7289e.png"
+  },
+  {
+    "revision": "add474185839d950b0635c01b34951b9",
+    "url": "/memorygame/static/media/goku.add47418.png"
+  },
+  {
+    "revision": "6982b4d62ded35e20b98ae54f4fc001c",
+    "url": "/memorygame/static/media/gokukid.6982b4d6.png"
+  },
+  {
+    "revision": "d88e6693492749f8e11398b6167617a4",
+    "url": "/memorygame/static/media/goten.d88e6693.png"
+  },
+  {
+    "revision": "acadba44b8079119c9d8bb67323e59e1",
+    "url": "/memorygame/static/media/kameHouse.acadba44.jpg"
+  },
+  {
+    "revision": "b012b2d0ac5b8dee73b996fcacc770b8",
+    "url": "/memorygame/static/media/trunks.b012b2d0.png"
+  }
+]);
